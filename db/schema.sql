eef80b932d244dcbd845bc3dec843816977ef52e
Pranith Gunda <pranith0527@yahoo.com>
2024-07-08 21:31:29 -0400

    Added scripts to create employee database, create tables for department, role and employee

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,31 @@ CREATE DATABASE employee_db;
 
 \c employee_db;
 
--- SQL Query to 
+-- SQL Query to create department table
+
+CREATE TABLE department(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(30)
+);
+
+-- SQL Query to create role table
+
+CREATE TABLE role(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department INTEGER,
+    FOREIGN KEY (department) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- SQL Query to create employee table
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+)
